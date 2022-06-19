fafa2f2b94834736eefbcdfad6e8ceff60c26144
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-06-19 13:04:56 +0430

    :fire: Footer Complete :fire:

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
-
 const Button = ({ btnName, classStyles, handleClick }) => (
-  <button type="button" className={`nft-gradient text-sm minlg:test-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${classStyles}`} onClick={handleClick}>
+  <button type="button" className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${classStyles}`} onClick={handleClick}>
     {btnName}
   </button>
 );
