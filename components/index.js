8d97986ca813baaaf01b9a5d8bd23e5c0ba4bf7b
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-06-22 13:52:59 +0430

    Home Page: Top Sellers part implemented, and CratorCard component created

--- components/index.js
+++ components/index.js
@@ -2,5 +2,6 @@ import Footer from './Footer';
 import Navbar from './Navbar';
 import Button from './Button';
 import Banner from './Banner.jsx';
+import CreatorCard from './CreatorCard';
 
-export { Footer, Navbar, Button, Banner };
+export { Footer, Navbar, Button, Banner, CreatorCard };
