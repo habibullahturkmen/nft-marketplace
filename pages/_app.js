44810d80ee9d3c4e22f42315448d46f6dd96276d
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-06-27 13:29:09 +0430

    NFTContext (React Context) added

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,23 @@
 import { ThemeProvider } from 'next-themes';
 import Script from 'next/script';
 
+import { NFTProvider } from '../context/NFTContext';
 import { Navbar, Footer } from '../components';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <div className="pt-65">
-        <Component {...pageProps} />
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-    <Script src="https://kit.fontawesome.com/d45b25ceeb.js" crossorigin="anonymous" />
-  </ThemeProvider>
+      <Script src="https://kit.fontawesome.com/d45b25ceeb.js" crossorigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
 );
 
 export default MyApp;
