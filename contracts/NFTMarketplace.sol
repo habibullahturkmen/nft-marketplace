e0dea3b0ddaaf2c1115c99d295a5ce88f5a110be
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-06-27 09:44:50 +0430

    fetchMarketItems function created

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -108,4 +108,26 @@ contract NFTMarketplace is ERC721URIStorage {
       payable(owner).transfer(listingPrice);
       payable(idToMarketItem[tokenId].seller).transfer(msg.value);
     }
+
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+      uint itemCount = _tokenIds.current();
+      uint unsoldItemCount = _tokenIds.current() - _itemsSold.current();
+      uint currentIndex = 0;
+
+      MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+
+      for (uint256 i = 0; i < itemCount; i++) {
+        if (idToMarketItem[i + 1].owner == address(this)) {
+          uint currentId = i + 1;
+
+          MarketItem storage currentItem = idToMarketItem[currentId];
+          
+          items[currentIndex] = currentItem;
+          
+          currentIndex += 1;
+        }
+      }
+      
+      return items;
+    }
 }
