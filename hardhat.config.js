2b16ec126c51dc39086d538b049b995a351f6ac4
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-08-24 11:57:54 +0430

    goerli network added to hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,21 @@
-const fs = require('fs');
 require('@nomiclabs/hardhat-waffle');
 
-const privateKey = fs.readFileSync('.secret').toString().trim();
+const dotenv = require('dotenv');
+
+dotenv.config({ path: `${__dirname}/.env` });
+const { PRIVATE_KEY, RPC_URL } = process.env;
+
+const privateKey = PRIVATE_KEY;
+const rpcURL = RPC_URL;
 
 module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    goerli: {
+      url: rpcURL,
+      accounts: [`0x${privateKey}`],
     },
   },
   solidity: '0.8.4',
